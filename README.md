18bfd9557d762cd757344163673b6883f283d1ef
Rand Thacker <rand.thacker@gmail.com>
2018-12-04 22:51:10 -0600

    Getting basics of using Mongo with PHP

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-rtBandMate
-==========
+# rtBandMate
 
 Lyrics - tabs repository software for band members
 
 Plan:
-Be able to enter lyrics in a text format, maybe using a wysiwyg editor with some customized buttons.
--maybe a chord dropdown
-sort by artist or title
+* Be able to enter lyrics in a text format
+* maybe using a wysiwyg editor with some customized buttons.
+* maybe a chord dropdown
+* sort by artist or title
+* store info about special considerations for band members like:
+  * special instrument (indicating change)
+  * patch (i.e. Joel's vocal)
+  * track needed
+  
+
+# Details
+We'll need a database, and I'm thinking a mongo or other document database
+might be best suited, based on the fact that not every record will need all
+the fields, and if I think of new fields later, it'll be easy to add them.
+
+
+## database
---
+++ test.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: randt
+ * Date: 12/4/2018
+ * Time: 7:24 PM
+ */
+
+// This path should point to Composer's autoloader
+require 'vendor/autoload.php';
+
+use MongoDB\Client as Mongo;
+
+$config = parse_ini_file('config.ini', true);
+$database_info = $config['database'];
+
+$connect_string = sprintf('mongodb://%s:%d', $database_info['hostname'], $database_info['port']);
+$client = new Mongo($connect_string);
+
+/**
+ * @var MongoCollection
+ */
+$collection = $client->{$database_info['database']}->{$database_info['collection']};
+/* HOW TO INSERT
+$result = $collection->insertOne( [ 'first_name' => 'Rand', 'last_name' => 'Thacker' ] );
+$result = $collection->insertOne( [ 'first_name' => 'Joel', 'last_name' => 'DeLuna' ] );
+$result = $collection->insertOne( [ 'first_name' => 'Tom', 'last_name' => 'Flygare' ] );
+$result = $collection->insertOne( [ 'first_name' => 'Jason', 'last_name' => 'Zenda' ] );
+*/
+$result = $collection->find();
+
+foreach ($result as $entry) {
+    printf("%s: %s %s\n", $entry['_id'], $entry['first_name'], $entry['last_name']);
+}
+
+function test_insert()
+{
+
+}
